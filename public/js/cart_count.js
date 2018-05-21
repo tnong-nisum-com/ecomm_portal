7fe3e75b2723123634141a8bbecf9e5160c21aab
tnong <tnong@Admins-MacBook-Pro-2.local>
2018-05-21 10:59:18 -0700

    log checkout

--- public/js/cart_count.js
+++ public/js/cart_count.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
             itemList[OrderForm.items[i].id] = OrderForm.items[i].quantity;
             quantity += OrderForm.items[i].quantity;
          } 
-
+console.log(quantity);return false;
          quantity = quantity*1;
          updateCartCount(quantity);
     });
@@ -21,37 +21,6 @@ $(document).ready(function(){
         $('.cart-info .items').text(string);
         //update global count
         bagCount = quantity;
-    };
-    
-    var bridgeCORS = function(iLink, itemObj){
-        var url = 'https://216.218.200.215/cart.php';
-        var vtexLink = iLink.split('?')[0];
-        var data = qryStringParse(iLink);
-
-        //update item quantity
-        if(itemObj[data.sku] !== undefined){
-            //update item quantity
-            data.qty = itemObj[data.sku] + 1;
-        }else{
-            data.qty = 1;
-        }
-
-        //save as string so can be ajax-ed
-        data = JSON.stringify(data);
-
-        $.ajax({
-            url: url,
-            data: 'data=' + data,
-            dataType: 'html',
-            type: 'post',
-            success: function(html){
-                //update current state of the cart
-                //update current state of the cart
-                var count = $('.cart-info .items').text().match(/[0-9]/g)*1;
-                count++;
-                updateCartCount(count);
-            }
-        });
     };
 });
 
