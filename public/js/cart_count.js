faa5181c5cdf368cb3f466c65a00cb35517a3325
Ubuntu User <ubuntu@ubuntu-web.cs1cloud.internal>
2018-05-25 20:58:10 +0000

    added counter

---
+++ public/js/cart_count.js
@@ -0,0 +1,16 @@
+var itemList = {};
+var quantity = 0;
+
+$(function(){
+
+        vtexjs.checkout.getOrderForm()
+            .done(function(OrderForm){
+var quantity = 0;
+                for(var i = 0; i < OrderForm.items.length; i++){
+                    itemList[OrderForm.items[i].id] = OrderForm.items[i].quantity;
+                    quantity += OrderForm.items[i].quantity;
+                 }
+                quantity = quantity*1;
+    $('.cart-info .items').text(quantity);
+            });
+});
