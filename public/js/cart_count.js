0bee74d5bdc87caa1e3ee554b80c05548820a73b
Ubuntu User <ubuntu@ubuntu-web.cs1cloud.internal>
2018-05-25 20:45:32 +0000

    fixed cart count

--- public/js/cart_count.js
+++ public/js/cart_count.js
@@ -2,7 +2,16 @@ var itemList = {};
 var bagCount = 0;
 var quantity = 0; 
 
-$(document).ready(function(){
-    var count = $('#shop-cart p.cart-info span:nth-child(4)').text()*1;
-    $('.cart-info .items').text(count);
+$(function(){
+
+	vtexjs.checkout.getOrderForm()
+            .done(function(OrderForm){
+var quantity = 0;
+                for(var i = 0; i < OrderForm.items.length; i++){
+                    itemList[OrderForm.items[i].id] = OrderForm.items[i].quantity;
+                    quantity += OrderForm.items[i].quantity;
+                 } 
+		quantity = quantity*1;
+    $('.cart-info .items').text(quantity);
+            });
 });
