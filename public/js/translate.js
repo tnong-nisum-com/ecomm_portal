852651b9676b9216cb22d2fc5058de7f5a77942a
tnong <tnong@Admins-MacBook-Pro-2.local>
2018-05-31 14:14:45 -0700

    disable click to compare on error

--- public/js/translate.js
+++ public/js/translate.js
@@ -4,4 +4,11 @@ $('.btn-comparar').text('Compare');
 //Translate cart
 $('.add a').text('Add');
 
+//Circumvent alert button and replace content with English
+$(document).ready(function(){
+    $('.btn-comparar').on('click', function(e){
+        e.preventDefault();
+    });
+});
+
 
