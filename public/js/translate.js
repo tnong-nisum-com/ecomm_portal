a760d67c34e2ee9082feff676eb566688580f163
tnong <tnong@Admins-MacBook-Pro-2.local>
2018-05-31 15:21:38 -0700

    translation for product page

--- public/js/translate.js
+++ public/js/translate.js
@@ -4,6 +4,12 @@ $('.btn-comparar, .buy-button').text('Compare');
 //Translate cart
 $('.add a').text('Add');
 
+$(document).ready(function(){
+    $('.product-description .btn-group a:first-child').text('Product Description');
+    $('.product-description .btn-group a:nth-child(2)').text('Product Specifications');
+    $('.product-description .btn-group a:nth-child(3)').text('Reviews');
+});
+
 //Circumvent alert button and replace content with English
 /*
 $(document).ready(function(){
