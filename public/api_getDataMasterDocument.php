2c0cfd6c262117554e5d849ee7ec7dfd16564014
tnong <tnong@Admins-MacBook-Pro-2.local>
2018-06-01 15:28:48 -0700

    added back document id

--- public/api_getDataMasterDocument.php
+++ public/api_getDataMasterDocument.php
@@ -4,13 +4,13 @@ $entity = $_POST['entity'];
 $document_id = $_POST['id'];
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://nisumusa.vtexcommercestable.com.br/api/dataentities/" . $entity . "/documents/",// . $document_id,
+  CURLOPT_URL => "https://nisumusa.vtexcommercestable.com.br/api/dataentities/" . $entity . "/documents/" . $document_id,
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_CUSTOMREQUEST => "GET",
   CURLOPT_HTTPHEADER => array(
     "Content-Type: application/json",
     "X-VTEX-API-AppKey: vtexappkey-nisumusa-KMTWAB",
