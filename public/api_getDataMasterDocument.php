ad5a4b80650497f8fb855d37fa02b61b453cf0a6
tnong <tnong@Admins-MacBook-Pro-2.local>
2018-06-01 14:38:10 -0700

    added data master document

---
+++ public/api_getDataMasterDocument.php
@@ -0,0 +1,30 @@
+<?php
+$curl = curl_init();
+$entity = $_POST['entity'];
+$document_id = $_POST['id'];
+print_r($_POST);die;
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://nisumusa.vtexcommercestable.com.br/api/dataentities/" . $entity . "/documents/" . $document_id,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "Content-Type: application/json",
+    "X-VTEX-API-AppKey: vtexappkey-nisumusa-KMTWAB",
+    "X-VTEX-API-AppToken: BPOZNSBZNJCSBKOZNQKFYBSBTQHRJQJAZLGLXFYEHABGPDUPSGYZOJCAXMGWLKQWABSQDDBISFXYZLMPWRYCSJLQIGOKYBTXJFGNLEMPFPHIMDALAQATLOEQPCHWORKQ"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
